f2c8e9419ee8899e391fff25830f5a242f36d676
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-10 23:13:04 +0500

    Add URLs constants for API and main site base URLs

---
+++ src/constants/urls.js
@@ -0,0 +1,6 @@
+const urls = {
+    apiBaseUrl: 'https://api.howdy.uz/api/v1/', // Backend API base URL (update for production)
+    mainSiteUrl: 'https://prime.howdy.uz', // Main e-commerce site URL (update for production)
+};
+
+export default urls;
