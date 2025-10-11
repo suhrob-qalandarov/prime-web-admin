9faf67a1426cbca6899ca12fcb819709549f3c14
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-11 10:34:50 +0500

    Update Dashboard route and page-links: add `/dashboard` path and adjust constants for consistency

--- src/app.jsx
+++ src/app.jsx
@@ -11,6 +11,7 @@ const App = () => {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Dashboard />}/>
+                <Route path="/dashboard" element={<Dashboard />}/>
             </Routes>
             {!hideFooterPages.includes(location.pathname) && <Footer />}
         </div>
--- src/constants/page-links.js
+++ src/constants/page-links.js
@@ -1,7 +1,7 @@
 import {faBoxOpen, faChartLine, faCloudUploadAlt, faTags, faUsers} from "@fortawesome/free-solid-svg-icons";
 
 export const links = [
-    { text: 'Dashboard', icon: faChartLine, href: '/', active: true },
+    { text: 'Dashboard', icon: faChartLine, href: '/dashboard', active: true },
     { text: 'Buyurtmalar', icon: faTags, href: '/orders' },
     { text: 'Kategoriyalar', icon: faTags, href: '/categories' },
     { text: 'Mahsulotlar', icon: faBoxOpen, href: '/products' },
