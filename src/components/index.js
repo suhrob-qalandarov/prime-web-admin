847692af1f09a3744717295f181f6cf437034c94
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-11 09:38:52 +0500

    Add Sidebar export to components index for streamlined imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Navbar} from "./bars/navbar/navbar";
 export {default as Footer} from "./bars/footer/footer";
 export {default as Dashboard} from "./pages/dashboard/dashboard";
+export {default as Sidebar} from "./bars/sidebar/sidebar";
