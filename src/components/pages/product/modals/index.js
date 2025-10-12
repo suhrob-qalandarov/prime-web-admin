9418e319b2f433a55b614ab42672d86be881e6a6
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-13 01:23:32 +0500

    Gather all components to index.js

---
+++ src/components/pages/product/modals/index.js
@@ -0,0 +1,6 @@
+export {default as EditMenuModal} from "./edit-menu";
+export {default as AddModal} from "./add";
+export {default as ViewModal} from "./view";
+export {default as StatusEditModal} from "./status-edit";
+export {default as PicturesEditModal} from "./pictures-edit";
+//export {default as Modal} from "/";
