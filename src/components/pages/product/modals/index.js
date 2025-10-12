296ab9859279a3dc424b503d32cfe1d91308bcd0
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-13 01:42:42 +0500

    Gather new components add-size, edit modals to index.js

--- src/components/pages/product/modals/index.js
+++ src/components/pages/product/modals/index.js
@@ -1,6 +1,7 @@
-export {default as EditMenuModal} from "./edit-menu";
 export {default as AddModal} from "./add";
 export {default as ViewModal} from "./view";
+export {default as EditModal} from "./edit";
+export {default as EditMenuModal} from "./edit-menu";
 export {default as StatusEditModal} from "./status-edit";
 export {default as PicturesEditModal} from "./pictures-edit";
-//export {default as Modal} from "/";
+export {default as AddSizeModal} from "./add-size";
