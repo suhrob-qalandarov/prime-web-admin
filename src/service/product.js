482370fca8645b2295a6b7c6bb536447ffad125c
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-17 21:07:21 +0500

    Fixed get one product data, it get product data from local storage

--- src/service/product.js
+++ src/service/product.js
@@ -39,14 +39,8 @@ const ProductService = {
 
     // Get single product by id, in ls
     async getById(id) {
-        /*try {
-            const response = await axios(`/api/products/${id}`)
-            if (!response.ok) throw new Error("Failed to fetch product")
-            return await response.json()
-        } catch (error) {
-            console.error("Error fetching product:", error)
-            throw error
-        }*/
+        let products = JSON.parse(localStorage.getItem("products"))
+        return products.filter(item => item.id !== id)
     },
 
     // Persist new product data to backend
