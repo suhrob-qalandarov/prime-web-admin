c89caa3a82ded97f50c3f1ca6ecca17c313cea3f
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-21 19:30:38 +0500

    Page list reordered

--- src/constants/page-links.js
+++ src/constants/page-links.js
@@ -2,10 +2,10 @@ import { faChartLine, faShoppingBag, faFolder, faBoxOpen, faUsers, faCloudUpload
 
 export const links = [
     { text: 'Dashboard', icon: faChartLine, href: '/dashboard' },
+    { text: 'Ombor', icon: faWarehouse, href: '/warehouse' },
     { text: 'Buyurtmalar', icon: faShoppingBag, href: '/orders' },
     { text: 'Kategoriyalar', icon: faFolder, href: '/categories' },
     { text: 'Mahsulotlar', icon: faBoxOpen, href: '/products' },
-    { text: 'Ombor', icon: faWarehouse, href: '/warehouse' },
     { text: 'Foydalanuvchilar', icon: faUsers, href: '/users' },
     { text: 'Fayllar', icon: faCloudUploadAlt, href: '/files' },
 ];
