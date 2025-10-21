a393252054101feb6e2ea5e20e472e6b1107b936
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-21 10:46:53 +0500

    Updated inventory icon

--- src/constants/page-links.js
+++ src/constants/page-links.js
@@ -1,11 +1,11 @@
-import { faChartLine, faShoppingBag, faFolder, faBoxOpen, faUsers, faCloudUploadAlt } from "@fortawesome/free-solid-svg-icons";
+import { faChartLine, faShoppingBag, faFolder, faBoxOpen, faUsers, faCloudUploadAlt, faWarehouse } from "@fortawesome/free-solid-svg-icons";
 
 export const links = [
     { text: 'Dashboard', icon: faChartLine, href: '/dashboard' },
     { text: 'Buyurtmalar', icon: faShoppingBag, href: '/orders' },
     { text: 'Kategoriyalar', icon: faFolder, href: '/categories' },
     { text: 'Mahsulotlar', icon: faBoxOpen, href: '/products' },
-    { text: 'Ombor', icon: faBoxOpen, href: '/warehouse' },
+    { text: 'Ombor', icon: faWarehouse, href: '/warehouse' },
     { text: 'Foydalanuvchilar', icon: faUsers, href: '/users' },
     { text: 'Fayllar', icon: faCloudUploadAlt, href: '/files' },
 ];
